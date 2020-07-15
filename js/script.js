3bcfc7611ba22101c84b6435fceb1799851b98df
Al666ex <alexss@list.ru>
2020-07-15 10:30:37 +0300

    functions completed

--- js/script.js
+++ js/script.js
@@ -19,33 +19,54 @@ let personalMovieDb = {
     privat : false
 };
 
-
-for(let i = 0; i < 2 ; i++){
-    const a = prompt("Введите наименование фильма",''),
-          b = prompt("Оцените фильм","");
-
-    if((a !== null) && (b !== null) && (a !== "") && (b !== '') && (a.length < 50) ){
-        personalMovieDb.movie[a] = b;
-        
-        console.log("cool");
-    } else {
-        console.log("bad");
-        i--;
+function rememberMyFilms(){
+    for(let i = 0; i < 2 ; i++){
+        const a = prompt("Введите наименование фильма",''),
+              b = prompt("Оцените фильм","");
+    
+        if((a !== null) && (b !== null) && (a !== "") && (b !== '') && (a.length < 50) ){
+            personalMovieDb.movie[a] = b;
+            
+            console.log("cool");
+        } else {
+            console.log("bad");
+            i--;
+        }
     }
 }
 
-console.log(personalMovieDb);
+//rememberMyFilms();
 
-if(personalMovieDb.count < 10){
-    console.log("посмотрели мало фильмов");
-} else if(personalMovieDb.count >= 10 && personalMovieDb.count <30){
-    console.log("Ds классический зритель");
-} else if(personalMovieDb.count > 30){
-    console.log("Вы киноман");
-} else {console.log("Error");}
+function detectPersonalLevel(){
+    if(personalMovieDb.count < 10){
+        console.log("посмотрели мало фильмов");
+    } else if(personalMovieDb.count >= 10 && personalMovieDb.count <30){
+        console.log("Ds классический зритель");
+    } else if(personalMovieDb.count > 30){
+        console.log("Вы киноман");
+    } else {console.log("Error");}
+    
+}
+//detectPersonalLevel();
+
+function showMyDb(hide){
+    if(!hide){
+        console.log(personalMovieDb);
+    }
+}
+
+showMyDb(personalMovieDb.privat);
 
 
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        const genre = prompt(`Ваш любимый жанр ${i}`,'');
+        if((genre !== '') && (genre !== null)){
+            personalMovieDb.genres[i-1] = genre;
+        } else {
+            i--;
+        }
+    }
+}
 
-
-
-
+writeYourGenres();
