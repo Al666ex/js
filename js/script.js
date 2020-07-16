ec9d56b878ac07c47dc8ce4a9b74ea50d53b9bfd
Alexandr Stavschi <stavschi@olimp.registru.md>
2020-07-16 14:31:22 +0300

    full object

--- js/script.js
+++ js/script.js
@@ -1,72 +1,68 @@
-
-
-let numberOfFilms;
-
-function start(){
-    numberOfFilms = +prompt('how many films you watched?','');
-    while(numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('how many films you watched?','');
-    }
-}
-
-//start();
+"use strict";
 
 let personalMovieDb = {
-    count : numberOfFilms,
+    count : 0,
     movie : {},
     actors : {},
     genres : [],
-    privat : false
+    privat : false,
+    start : function(){
+        personalMovieDb.count = +prompt('how many films you watched?','');
+        while(personalMovieDb.count === '' || personalMovieDb.count === null || isNaN(personalMovieDb.count)){
+            personalMovieDb.count = +prompt('how many films you watched?','');
+        }
+    
+    },
+    rememberMyFilms : function(){
+        for(let i = 0; i < 2 ; i++){
+            const a = prompt("Введите наименование фильма",''),
+                  b = prompt("Оцените фильм","");
+        
+            if((a !== null) && (b !== null) && (a !== "") && (b !== '') && (a.length < 50) ){
+                personalMovieDb.movie[a] = b;
+                
+                console.log("cool");
+            } else {
+                console.log("bad");
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel : function(){
+        if(personalMovieDb.count < 10){
+            console.log("посмотрели мало фильмов");
+        } else if(personalMovieDb.count >= 10 && personalMovieDb.count <30){
+            console.log("Ds классический зритель");
+        } else if(personalMovieDb.count > 30){
+            console.log("Вы киноман");
+        } else {console.log("Error");}        
+    },
+    showMyDb : function(hide){
+        if(!hide){
+            console.log(personalMovieDb);
+        }       
+    },    
+    writeYourGenres : function(){
+        for(let i = 1; i <= 3; i++){
+            const genre = prompt(`Ваш любимый жанр ${i}`,'');
+            if((genre !== '') && (genre !== null)){
+                personalMovieDb.genres[i-1] = genre;
+            } else {
+                console.log("некорректные данные")
+                i--;
+            }
+        }
+        personalMovieDb.genres.forEach(function(item,index){
+            console.log(`номер ${index+1} жанр ${item} `)
+        })
+    },
+    toggleVisibleMyDB : function(){
+        if(personalMovieDb.privat) {
+            personalMovieDb.privat = false
+        }else{personalMovieDb.privat = true}
+    }
+
 };
 
-function rememberMyFilms(){
-    for(let i = 0; i < 2 ; i++){
-        const a = prompt("Введите наименование фильма",''),
-              b = prompt("Оцените фильм","");
-    
-        if((a !== null) && (b !== null) && (a !== "") && (b !== '') && (a.length < 50) ){
-            personalMovieDb.movie[a] = b;
-            
-            console.log("cool");
-        } else {
-            console.log("bad");
-            i--;
-        }
-    }
-}
-
-//rememberMyFilms();
-
-function detectPersonalLevel(){
-    if(personalMovieDb.count < 10){
-        console.log("посмотрели мало фильмов");
-    } else if(personalMovieDb.count >= 10 && personalMovieDb.count <30){
-        console.log("Ds классический зритель");
-    } else if(personalMovieDb.count > 30){
-        console.log("Вы киноман");
-    } else {console.log("Error");}
-    
-}
-//detectPersonalLevel();
-
-function showMyDb(hide){
-    if(!hide){
-        console.log(personalMovieDb);
-    }
-}
-
-//showMyDb(personalMovieDb.privat);
-
-
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        const genre = prompt(`Ваш любимый жанр ${i}`,'');
-        if((genre !== '') && (genre !== null)){
-            personalMovieDb.genres[i-1] = genre;
-        } else {
-            i--;
-        }
-    }
-}
-
-//writeYourGenres();
+console.log(personalMovieDb.toggleVisibleMyDB())
+console.log(personalMovieDb.toggleVisibleMyDB())
