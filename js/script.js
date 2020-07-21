12c580c176042f5a36b49226dfe0f89d8fc6ead5
Al666ex <alexss@list.ru>
2020-07-21 21:36:26 +0300

    movies

--- js/script.js
+++ js/script.js
@@ -1,102 +1,49 @@
-"use strict";
+/* Задания на урок:
 
-let personalMovieDb = {
-    count : 0,
-    movie : {},
-    actors : {},
-    genres : [],
-    privat : false,
-    start : function(){
-        personalMovieDb.count = +prompt('how many films you watched?','');
-        while(personalMovieDb.count === '' || personalMovieDb.count === null || isNaN(personalMovieDb.count)){
-            personalMovieDb.count = +prompt('how many films you watched?','');
-        }
-    
-    },
-    rememberMyFilms : function(){
-        for(let i = 0; i < 2 ; i++){
-            const a = prompt("Введите наименование фильма",''),
-                  b = prompt("Оцените фильм","");
-        
-            if((a !== null) && (b !== null) && (a !== "") && (b !== '') && (a.length < 50) ){
-                personalMovieDb.movie[a] = b;
-                
-                console.log("cool");
-            } else {
-                console.log("bad");
-                i--;
-            }
-        }
-    },
-    detectPersonalLevel : function(){
-        if(personalMovieDb.count < 10){
-            console.log("посмотрели мало фильмов");
-        } else if(personalMovieDb.count >= 10 && personalMovieDb.count <30){
-            console.log("Ds классический зритель");
-        } else if(personalMovieDb.count > 30){
-            console.log("Вы киноман");
-        } else {console.log("Error");}        
-    },
-    showMyDb : function(hide){
-        if(!hide){
-            console.log(personalMovieDb);
-        }       
-    },    
-    writeYourGenres : function(){
-        for(let i = 1; i < 2; i++){
-            // const genre = prompt(`Ваш любимый жанр ${i}`,'');
-            // if((genre !== '') && (genre !== null)){
-            //     personalMovieDb.genres[i-1] = genre;
-            // } else {
-            //     console.log("некорректные данные")
-            //     i--;
-            // }
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-            const genres = prompt(`Ваши любимые жанры кино введите через запятую`,'').toLowerCase();
-            if((genres !== "") && (genres !== null)){
-                personalMovieDb.genres = genres.split(", ")
-                personalMovieDb.genres.sort()
-            }else {
-                console.log("данные введены не корректно");
-                i--;                
-            }
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
 
-        }
-        personalMovieDb.genres.forEach(function(item,index){
-            console.log(`номер ${index+1} жанр ${item} `)
-        })
-    },
-    toggleVisibleMyDB : function(){
-        if(personalMovieDb.privat) {
-            personalMovieDb.privat = false
-        }else{personalMovieDb.privat = true}
-    }
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
 
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
 
+const promoImg = document.querySelectorAll(".promo__adv img"),
+      genre = document.querySelector('.promo__genre'),
+      listMovies = document.querySelector('.promo__interactive-list'),
+      moviesDel = listMovies.querySelectorAll('.promo__interactive-item');
+;
 
-let one = document.querySelectorAll(".one")
-//one.forEach(item => console.log(item))
+promoImg.forEach(item =>
+    item.remove()
+);
 
-let box = document.getElementById("box")
-let tagName = document.getElementsByTagName("button")
-let hearts = document.querySelectorAll(".heart")
-let wrapper = document.querySelector(".wrapper")
-let circle = document.querySelectorAll(".circle")
-
-//circle[1].remove()
-
-
-hearts.forEach(item => item.style.background = "green")
+genre.textContent = 'драма';
 
 
+moviesDel.forEach(movie => 
+    movie.remove()
+);
 
-let div = document.createElement("div");
-div.innerHTML = "<h1>Hello world</h1>"
-div.classList.add("black");
-//hearts[1].after(div)
-
-wrapper.prepend(div)
-//document.body.append(div)
-div.insertAdjacentHTML("afterend","<h1>Testing...</h1>")
+movieDB.movies.forEach((item,index) => {
+    listMovies.innerHTML +=
+    `<li class="promo__interactive-item">${index + 1} ${item}
+                            <div class="delete"></div>
+    </li>`;
+})
