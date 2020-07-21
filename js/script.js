fe83849129607573db0958f39a1c7b02f6c40ee9
Al666ex <alexss@list.ru>
2020-07-21 22:32:32 +0300

    add movie__

--- js/script.js
+++ js/script.js
@@ -27,7 +27,9 @@ const movieDB = {
 const promoImg = document.querySelectorAll(".promo__adv img"),
       genre = document.querySelector('.promo__genre'),
       listMovies = document.querySelector('.promo__interactive-list'),
-      moviesDel = listMovies.querySelectorAll('.promo__interactive-item');
+      moviesDel = listMovies.querySelectorAll('.promo__interactive-item'),
+      inputMovie = document.querySelector('.promo__interactive .add input[type="text"]'),
+      buttonMovie = document.querySelector('.promo__interactive .add button');
 ;
 
 promoImg.forEach(item =>
@@ -44,6 +46,21 @@ moviesDel.forEach(movie =>
 movieDB.movies.forEach((item,index) => {
     listMovies.innerHTML +=
     `<li class="promo__interactive-item">${index + 1} ${item}
-                            <div class="delete"></div>
+        <div class="delete"></div>
     </li>`;
-})
+});
+
+buttonMovie.addEventListener('click', function(e){
+    e.preventDefault();    
+    movieDB.movies.push(inputMovie.value);    
+        listMovies.innerHTML +=
+        `<li class="promo__interactive-item">${movieDB.movies.length} ${inputMovie.value}
+            <div class="delete"></div>
+        </li>`;    
+    movieDB.movies.forEach(item => console.log(item));
+    inputMovie.value = null;
+});
+
+
+
+
