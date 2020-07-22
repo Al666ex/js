6aa43f4099320fc89ba26b1d6dc5d7283ffd56f6
Alexandr Stavschi <stavschi@olimp.registru.md>
2020-07-22 13:38:49 +0300

    shablon movie

--- js/script.js
+++ js/script.js
@@ -13,47 +13,46 @@
 5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
-
+window.addEventListener('DOMContentLoaded', () => {
     const movieDB = {
         movies: [
             "Логан",
             "Лига справедливости",
             "Ла-ла лэнд",
             "Одержимость",
-            "Скотт Пилигрим против..."          
+            "Скотт Пилигрим против..."                      
         ]
     };
     
-    const promoImg = document.querySelectorAll(".promo__adv img"),
-          genre = document.querySelector('.promo__genre'),
-          listMovies = document.querySelector('.promo__interactive-list'),
-          moviesDel = listMovies.querySelectorAll('.promo__interactive-item'),
-          inputMovie = document.querySelector('.promo__interactive .add input[type="text"]'),
-          loveMovie = document.querySelector('.promo__interactive .add input[type="checkbox"]'),
-          buttonMovie = document.querySelector('.promo__interactive .add button'),
-          deleteSelectedMovie = document.querySelectorAll('.promo__interactive-item .delete');
-    ;
+    const adv = document.querySelectorAll(".promo__adv img"),
+          movieList = document.querySelector('.promo__interactive-list'),
+          poster = document.querySelector('.promo__bg'),
+          genre = document.querySelector('.promo__genre');
     
     
-    promoImg.forEach(item =>
+    adv.forEach(item =>
         item.remove()
     );
     
     genre.textContent = 'драма';
+    poster.style.backgroundImage = 'url("img/bg.jpg")'
+    movieList.innerHTML=''
     
-    moviesDel.forEach(movie => 
-        movie.remove()
-    );
+    // moviesDel.forEach(movie => 
+    //     movie.remove()
+    // );
     
     movieDB.movies.forEach((item,index) => {
         
-        listMovies.innerHTML +=
+        movieList.innerHTML +=
         `<li class="promo__interactive-item">${index + 1} ${item}
             <div class="delete"></div>
         </li>`;
     });
     
 
+})
+    
     
 
 
